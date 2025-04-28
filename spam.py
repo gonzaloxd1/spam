f8c2c0ac8c464b4705b178e9f4dfc6f0f95b085f
gonzaloxd1 <isafwck@gmail.com>
2025-04-28 14:19:23 -0700

    Update spam.py

--- spam.py
+++ spam.py
@@ -6,13 +6,15 @@ from time import sleep
 from telethon.tl import types
 from telethon.errors import FloodWaitError
 from telethon.errors import FloodWaitError, ChatAdminRequiredError
-api_id = '29348999'
-api_hash = '4db5414664d8ff25568166cfc014a56c'
+api_id = '28906639'
+api_hash = 'd0fc2d177ea67be7d7c138a5a80000ae'
 
 
-grupo_origen_id = -4172484199 #<----AQUI EL GRUPO  DE ORIGEN 
+grupo_origen_id = -4625080061 #<----AQUI EL GRUPO  DE ORIGEN 
 
-tu_numero_telefono = '+51998229537'
+tu_numero_telefono = '+51908618587'
+
+
 
 def iniciar_sesion():
     client = TelegramClient('session_name', api_id, api_hash)
